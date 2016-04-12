47a35a857a2c2676082680f4e7e7c23874a31d4e
Surya Bhupatiraju <surya95@gmail.com>
2016-04-12 15:50:28 -0400

    Fixed ParallelTable usage and found input dims

--- train.lua
+++ train.lua
@@ -1,43 +1,50 @@
-require 'nn'
 require 'torch'
+require 'nn'
+require 'nnx'
 require 'optim'
+require 'xlua'
+require 'image'
 
-noutputs = 30
+noutputs = 50
 
 nfeats = 3
-width = 32 
-height = 32
+width = 210
+height = 320
 ninputs = nfeats*width*height
 
 nstates = {64, 64, 128}
+dims = {49, 77}
 filtsize = 5
-poolsize = 2
-
+poolsize = 2 
 
 function create_model()
 	print('==> creating model')
 	local model = nn.Sequential()
 
 	local convnet = nn.Sequential()
-	convnet:add(nn.SpatialFullConvolution(nfeats, nstates[1], filtsize, filtsize))
+	convnet:add(nn.SpatialConvolution(nfeats, nstates[1], filtsize, filtsize))
 	convnet:add(nn.ReLU())
 	convnet:add(nn.SpatialMaxPooling(poolsize, poolsize, poolsize, poolsize))
 
-	convnet:add(nn.SpatialFullConvolution(nstates[1], nstates[2], filtsize, filtsize))
+	convnet:add(nn.SpatialConvolution(nstates[1], nstates[2], filtsize, filtsize))
 	convnet:add(nn.ReLU())
 	convnet:add(nn.SpatialMaxPooling(poolsize, poolsize, poolsize, poolsize))
 
-	convnet:add(nn.View(nstates[2]*filtsize*filtsize))
+	convnet:add(nn.View(nstates[2]*dims[1]*dims[2]))
+
 	convnet:add(nn.Dropout(0.5))
-	convnet:add(nn.Linear(nstates[2]*filtsize*filtsize, nstates[3]))
+	convnet:add(nn.Linear(nstates[2]*dims[1]*dims[2], nstates[3]))
+	convnet:add(nn.ReLU())
 	convnet:add(nn.Linear(nstates[3], noutputs)) 
 
 	local onehot = nn.Sequential()
 	onehot:add(nn.Linear(9, 9)) 
 
-	model:add(nn.ParallelTable())
-	model:add(onehot)
-	model:add(convnet)
+	local parallel = nn.ParallelTable()
+	parallel:add(onehot)
+	parallel:add(convnet)
+
+	model:add(parallel)
 	model:add(nn.JoinTable(1))
 
 	-- Add more layers here for deconvolution
@@ -59,19 +66,31 @@ function get_frame_data(filename)
 
 end
 
-function make_training_data()
+function make_training_data(dir)
+	-- image.load(xxxxxx.png) yields a 3x210x320 image
 
 end
 
-function train()
+function test(model)
 
 end
 
-function test()
+function main()
+	model = create_model()
+	training_data = make_training_data()
 
+	-- training of autoencoder here
+
+	model:training() -- put into training mode (dropout turns on)
+
+	i = image.load("atari/exp1/000000.png")
+	j = torch.rand(9)
+
+	print(model:forward{j, i})
+	print(i:size()) 
 end
 
-model = create_model()
+main()
 
 --[[
 if opt.visualize then
